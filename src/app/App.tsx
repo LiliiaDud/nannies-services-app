09ea5fc621dc14e114b18244ce0e1fd9e4616a62
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-04-17 13:39:35 +0200

    feat: add Header and global modal handling (Login, Registration) to App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,37 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import css from './App.module.css';
+import { useModal } from "../components/ModalContext/UseModal";
+import { useState } from "react";
+
+import Header from "../components/Header/Header";
 import Home from "../pages/Home/Home.tsx";
-import Nannies from "../pages/Nannies/Nannies.tsx";
-import Favorites from "../pages/Favorites/Favorites.tsx";
+import Nannies from "../pages/Nannies/Nannies";
+import Favorites from "../pages/Favorites/Favorites";
+
+import Login from "../components/Login/Login";
+import Registration from "../components/Registration/Registration";
+import Modal from "../components/Modal/Modal";
 
   function App() {
+    const { closeModal, isModalOpen } = useModal();
+    const [modalType, setModalType] = useState<
+      "login" | "register" | "appointment" | null
+    >(null);
+
+    const location = useLocation();
+    const page = location.pathname;
+
     return (
       <div className={css.app_container}>
+        <Header setModalType={setModalType} page={page} />
+        {isModalOpen && (
+          <Modal onClose={closeModal}>
+            {modalType === "login" && <Login />}
+            {modalType === "register" && <Registration />}
+          </Modal>
+        )}
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/nannies" element={<Nannies />} />
